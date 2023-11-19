b65c73d47f6c44c4b49490b2ca7e4783829a1c2e
Nikolai Marchenkov <nikolai.marchenkov@mail.ru>
2023-11-19 16:51:58 +0300

    попытка

---
+++ train_python/lesson_10_sql/task.py
@@ -0,0 +1,31 @@
+from sqlalchemy import BigInteger,ForeignKey, Column,  Integer, MetaData, String, create_engine, select
+from sqlalchemy.orm import  registry, declarative_base, Session, mapped_column,Mapped
+
+engine = create_engine("sqlite:///:memory:",echo=True)
+Base = declarative_base()
+
+class AbstractModel(Base, object):
+    __tablename__ = 'AbstractModel'
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
+
+class UserModel(AbstractModel, Base):
+    __tablename__ = 'users'
+    user_id: Mapped[int] = mapped_column(BigInteger)
+    name: Mapped[str] = mapped_column()
+    fullname: Mapped[str] = mapped_column()
+
+class AdressModel(AbstractModel, Base):
+    __tablename__ = 'addressess'
+    email: Mapped[str] = mapped_column()
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+
+
+with Session(engine) as session:
+    with session.begin():
+        MetaData.create_all(engine)
+        user = UserModel(user_id = 1, name = "Kek", fullname = 'Full Name')
+        session.add(user)
+    with session.begin():
+        res = session.execute(select(UserModel).where(UserModel.id == 1 ))
+        print(res.scalar())
--- train_python/lesson_10_sql/task_2.py
+++ train_python/lesson_10_sql/task_2.py
@@ -1,13 +1,19 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import Column, Integer, MetaData, select, String, ForeignKey,Table, create_engine
+from sqlalchemy.orm import declarative_base, relationship,Session
 
 Base = declarative_base()
+engine = create_engine("sqlite:///:memory:",echo=True)
+metadata_obj = MetaData()
+
+association_table = Table('student_course_association', Base.metadata,
+    Column('courses_id', Integer, ForeignKey('students.id')),
+    Column('student_id', Integer, ForeignKey('courses.id')))
 
 class Student(Base):
     __tablename__ = 'students'
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    courses = relationship("Course", back_populates="student")
+    courses_id = relationship("Course", back_populates="student")
 
 class Course(Base):
     __tablename__ = 'courses'
@@ -15,3 +21,12 @@ class Course(Base):
     name = Column(String)
     student_id = Column(Integer, ForeignKey('students.id'))
     student = relationship("Student", back_populates="courses")
+
+    with Session(engine) as session:
+        with session.begin():
+            metadata_obj.create_all(engine)
+            stu = Student(id = 1, name = "Kek", courses_id = 1)
+            session.add(stu)
+        with session.begin():
+            res = session.execute(select(Student).where(Student.id == 1 ))
+            print(res.scalar())
